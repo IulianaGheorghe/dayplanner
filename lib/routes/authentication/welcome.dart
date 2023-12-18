68507a0a5811a74b8743a1aee278d7afc6895989
IulianaGheorghe <iuliana.gheorghe@student.upt.ro>
2023-12-18 20:04:47 +0200

    Add initial project files

---
+++ lib/routes/authentication/welcome.dart
@@ -0,0 +1,101 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+import '../../util/constants.dart';
+import 'log_in.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(todoPattern1),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.fromLTRB(0, 0, 170, 0),
+                child: Text("Day",
+                    style: TextStyle(
+                      fontSize: titleSize,
+                      fontFamily: font1,
+                      foreground: Paint()
+                        ..style = PaintingStyle.stroke
+                        ..strokeWidth = 3.0
+                        ..color = Colors.black,)
+                )
+              ),
+              Container(
+                margin: const EdgeInsets.fromLTRB(marginSize*6,0,50,0),
+                  child: Text("Planner",
+                      style: TextStyle(
+                        fontSize: titleSize,
+                        fontFamily: font1,
+                        foreground: Paint()
+                          ..style = PaintingStyle.stroke
+                          ..strokeWidth = 3.0
+                          ..color = Colors.black,)
+                  )
+              ),
+              const SizedBox(height: 150),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const LogIn()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                    shape: const StadiumBorder(),
+                    fixedSize: const Size(buttonWidth, buttonHeight),
+                    textStyle: const TextStyle(
+                        fontSize: buttonText,
+                        fontWeight: FontWeight.bold
+                    ),
+                    side: const BorderSide(color: buttonBorderColor, width: 2),
+                    backgroundColor: buttonColor,
+                    foregroundColor: buttonTextColor,
+                    elevation: 15
+                ),
+                child: const Text("Log in"),
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  // Navigator.push(context,
+                  //   MaterialPageRoute(builder: (context) => const SignUp()),
+                  // );
+                },
+                style: ElevatedButton.styleFrom(
+                    shape: const StadiumBorder(),
+                    fixedSize: const Size(buttonWidth, buttonHeight),
+                    textStyle: const TextStyle(
+                        fontSize: buttonText,
+                        fontWeight: FontWeight.bold
+                    ),
+                    side: const BorderSide(color: buttonBorderColor, width: 2),
+                    backgroundColor: buttonColor,
+                    foregroundColor: buttonTextColor,
+                    elevation: 15
+                ),
+                child: const Text("Sign up"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/util/constants.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+// Color constants
+const primaryColor = Color(0xff016e68);
+const secondColor = Color(0xffece84b);
+const buttonColor = Color(0xff24b1bd);
+const buttonTextColor = Colors.black;
+const buttonBorderColor = Color(0xff016e68);
+
+// App sizing
+const marginSize = 20.0;
+const titleSize = 50.0;
+const buttonHeight = 60.0;
+const buttonWidth = 300.0;
+const buttonText = 25.0;
+const pageSizeName = 30.0;
+
+const boxDataSize = 30.0; // boxes for sign in/log in pages
+
+// -- Fonts
+const String font1 = "Righteous";
+const String font2 = "Lobster";
+
+// Images
+const todoPattern1 = 'assets/images/todo pattern.jpg';
+const todoPattern2 = 'assets/images/todo_pattern2.jpg';
