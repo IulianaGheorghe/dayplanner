b24c3632d2b93bae1fa4cefea53978d1abac82e3
IulianaGheorghe <iuliana.gheorghe@student.upt.ro>
2024-05-27 23:38:47 +0300

    Add Account details

--- lib/screens/tabs/account.dart
+++ lib/screens/tabs/account.dart
@@ -1,4 +1,15 @@
+import 'dart:io';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+import '../../common_widgets/showSnackBar.dart';
+import '../../services/auth_methods.dart';
+import '../../util/constants.dart';
+import '../others/profile/edit_profile.dart';
+import 'home.dart';
 
 class Account extends StatefulWidget{
   const Account({super.key});
@@ -8,11 +19,133 @@ class Account extends StatefulWidget{
 }
 
 class _AccountState extends State<Account>{
+  String userName = "";
+  String userEmail = "";
+  String userPassword = "";
+  String userPhoto = "";
+  bool showPassword = true;
+  File? _imageFile;
+
+  late Future<Map<String, String>> fetchDetails;
+
+  FirebaseAuthMethods authMethods = FirebaseAuthMethods();
+
+  @override
+  void initState() {
+    super.initState();
+    fetchDetails = _getUserDetails();
+  }
+
+  Future<Map<String, String>> _getUserDetails() async {
+    User? user = FirebaseAuth.instance.currentUser;
+    String? email = user?.email;
+    if (email != null) {
+      String name = await authMethods.getName(email) ;
+      String photo = await authMethods.getPhoto(email);
+      setState(() {
+        userName = name;
+        userEmail = email;
+        userPhoto = photo;
+      });
+      return {
+        'userName': name,
+        'userEmail': email,
+        'userPhoto': photo,
+      };
+    }
+    return {};
+  }
+
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _imageFile = File(pickedFile.path);
+      });
+    }
+    else
+    {
+      showSnackBar(context, 'Image not added!');
+    }
+  }
+
+  Future _uploadImage() async {
+    try {
+      String userID = await authMethods.getUserId();
+      final postID = DateTime.now().millisecondsSinceEpoch.toString();
+      final storageReference = FirebaseStorage.instance.ref().child('$userID/profile').child("post_$postID");
+      await storageReference.putFile(_imageFile!);
+      final downloadUrl = await storageReference.getDownloadURL();
+      setState(() {
+        userPhoto = downloadUrl;
+      });
+    } catch (e) {
+      throw Exception('Error uploading image: $e');
+    }
+  }
+
+  void saveChanges() async {
+    User? user = FirebaseAuth.instance.currentUser;
+    String userID = await authMethods.getUserId();
+    if( _imageFile != null) {
+      await _uploadImage();
+    }
+    try {
+      if( _imageFile != null ) {
+        await authMethods.updatePhotoURL(userID, userPhoto);
+      }
+
+      if( userPassword != "") {
+        user?.updatePassword(userPassword);
+      }
+
+      if( userName != '') {
+        await authMethods.updateUsername(userID, userName);
+      }
+      Navigator.pop(context);
+      Navigator.pushReplacement<void, void>(
+        context,
+        MaterialPageRoute<void>(
+          builder: (BuildContext context) => const Home(),
+        ),
+      );
+    } catch (e) {
+      throw Exception('Error updating account details: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        popupMenuTheme: PopupMenuThemeData(
+          color: Colors.white.withOpacity(0.8),
+        ),
+      ),
       home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: profilePageColor,
+          actions: [
+            PopupMenuButton<String>(
+              onSelected: (value) {
+                if (value == 'Edit profile') {
+                  Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const EditProfile()));
+                }
+              },
+              itemBuilder: (BuildContext context) {
+                return [
+                  const PopupMenuItem<String>(
+                    value: 'Edit profile',
+                    child: Text('Edit profile')
+                  ),
+                ];
+              },
+              icon: const Icon(Icons.more_vert),
+            ),
+          ],
+        ),
         body: Stack(
           fit: StackFit.expand,
           children: [
@@ -20,18 +153,121 @@ class _AccountState extends State<Account>{
               'assets/images/t.jpg',
               fit: BoxFit.cover,
             ),
-            const Center(
-              child: Text(
-                "I have an account!",
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+            CustomScrollView(
+              slivers: <Widget>[
+                SliverFillRemaining(
+                  child: FutureBuilder<Map<String, String>>(
+                    future: fetchDetails,
+                    builder: (BuildContext context, AsyncSnapshot<Map<String, String>> snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                          child: CircularProgressIndicator(
+                            valueColor:AlwaysStoppedAnimation<Color>(profilePageColor),
+                          ),
+                        );
+                      } else if (snapshot.hasError) {
+                        return Text('Error: ${snapshot.error}');
+                      } else if (snapshot.hasData) {
+                        return buildProfile();
+                      } else {
+                        return const Text('No data available');
+                      }
+                    },
+                  ),
                 ),
-              ),
+              ],
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Container buildProfile(){
+    ImageProvider<Object> imageShowed;
+    if(_imageFile == null)
+    {
+      imageShowed = NetworkImage(userPhoto);
+    } else {
+      imageShowed = FileImage(_imageFile!);
+      userPhoto = 'photo';
+    }
+
+    return Container(
+      padding: const EdgeInsets.only(left: 16, bottom: 25, right: 16),
+      child: ListView(
+        children: [
+          Center(
+            child: Stack(
+              children: [
+                InkWell(
+                  onLongPress: () {  _pickImage(); },
+                  child: userPhoto == ""
+                      ? Container(
+                    width: 150,
+                    height: 150,
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                            width: 4,
+                            color: Theme.of(context).scaffoldBackgroundColor),
+                        boxShadow: [
+                          BoxShadow(
+                              spreadRadius: 2,
+                              blurRadius: 10,
+                              color: Colors.black.withOpacity(0.1),
+                              offset: const Offset(0, 10))
+                        ],
+                        shape: BoxShape.circle,
+                        image: const DecorationImage(
+                            fit: BoxFit.cover,
+                            image: AssetImage('assets/images/user.png'))),
+                  )
+                      : Container(
+                    width: 150,
+                    height: 150,
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                            width: 4,
+                            color: Theme.of(context).scaffoldBackgroundColor),
+                        boxShadow: [
+                          BoxShadow(
+                              spreadRadius: 2,
+                              blurRadius: 10,
+                              color: Colors.black.withOpacity(0.1),
+                              offset: const Offset(0, 10))
+                        ],
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: imageShowed)),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 20),
+          Center(
+            child: Text(
+              userName,
+              style: const TextStyle(
+                fontFamily: font1,
+                fontSize: 25,
+              ),
+            ),
+          ),
+          const SizedBox(height: 5),
+          Center(
+            child: Text(
+              userEmail,
+              style: const TextStyle(
+                fontFamily: font1,
+                fontSize: 20,
+              ),
+            ),
+          ),
+          const SizedBox(height: 15),
+
+        ],
       ),
     );
   }
