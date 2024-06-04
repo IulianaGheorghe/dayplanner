d8bb03ab655b9493bf62fd9d0d97c8e91210ddcc
IulianaGheorghe <iuliana.gheorghe@student.upt.ro>
2024-06-04 22:42:11 +0300

    Add category menu button

--- lib/screens/tabs/home.dart
+++ lib/screens/tabs/home.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import '../../common_widgets/showSnackBar.dart';
 import '../../services/task_services.dart';
 import '../../util/constants.dart';
 import '../others/task/add_task.dart';
@@ -52,7 +53,7 @@ class _HomeState extends State<Home>{
               fit: BoxFit.cover,
             ),
             Padding(
-              padding: const EdgeInsetsDirectional.fromSTEB(25, 40, 25, 40),
+              padding: const EdgeInsetsDirectional.fromSTEB(25, 40, 15, 40),
               child: Column (
                 children: [
                   Row(
@@ -76,59 +77,69 @@ class _HomeState extends State<Home>{
                       const Spacer(),
                       Column(
                         children: [
-                          SizedBox(
-                            width: MediaQuery.of(context).size.width / 2.9,
-                            child: ElevatedButton(
-                              onPressed: () {
-                                Navigator.push(context,
-                                    MaterialPageRoute(builder: (context) => const AddTask()));
-                              },
-                              style: ElevatedButton.styleFrom(
-                                  shape: const StadiumBorder(),
-                                  fixedSize: const Size(141.0, 55.0),
-                                  textStyle: const TextStyle(
-                                      fontSize: 13,
-                                      fontWeight: FontWeight.bold
-                                  ),
-                                  side: const BorderSide(color: buttonBorderColor, width: 1),
-                                  backgroundColor: primaryColor,
-                                  foregroundColor: buttonTextColor,
-                                  elevation: 10
-                              ),
-                              child: const Row(
-                                children: [
-                                  Icon(
-                                    Icons.add_circle,
-                                    color: Colors.black,
-                                    size: 30.0,
-                                  ),
-                                  Text(' New Task'),
-                                ],
+                          Padding(
+                            padding: const EdgeInsets.only(right: 15),
+                            child: SizedBox(
+                              width: MediaQuery.of(context).size.width / 2.8,
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  Navigator.push(context,
+                                      MaterialPageRoute(builder: (context) => const AddTask()));
+                                },
+                                style: ElevatedButton.styleFrom(
+                                    shape: const StadiumBorder(),
+                                    fixedSize: const Size(141.0, 55.0),
+                                    textStyle: const TextStyle(
+                                        fontSize: 13,
+                                        fontWeight: FontWeight.bold
+                                    ),
+                                    side: const BorderSide(color: buttonBorderColor, width: 1),
+                                    backgroundColor: primaryColor,
+                                    foregroundColor: buttonTextColor,
+                                    elevation: 10
+                                ),
+                                child: const Row(
+                                  children: [
+                                    Icon(
+                                      Icons.add_circle,
+                                      color: Colors.black,
+                                      size: 30.0,
+                                    ),
+                                    Text(
+                                      ' New Task',
+                                      style: TextStyle(
+                                        fontSize: 14
+                                      ),
+                                    ),
+                                  ],
+                                ),
                               ),
                             ),
                           ),
                           const SizedBox(height: 15),
-                          Align(
-                            alignment: Alignment.centerRight,
-                            child: SizedBox(
-                              width: MediaQuery.of(context).size.width / 2.8,
-                              child: DropdownButtonFormField(
-                                isExpanded: true,
-                                dropdownColor: Colors.white.withOpacity(0.9),
-                                decoration: InputDecoration(
-                                  fillColor: Colors.white.withOpacity(0.5),
-                                  filled: true,
-                                  enabledBorder: OutlineInputBorder(
-                                    borderSide: const BorderSide(color: Colors.transparent),
-                                    borderRadius: BorderRadius.circular(30),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              SizedBox(
+                                width: MediaQuery.of(context).size.width / 2.8,
+                                child: DropdownButtonFormField(
+                                  isExpanded: true,
+                                  dropdownColor: Colors.white.withOpacity(0.9),
+                                  decoration: InputDecoration(
+                                    fillColor: Colors.white.withOpacity(0.5),
+                                    filled: true,
+                                    enabledBorder: OutlineInputBorder(
+                                      borderSide: const BorderSide(color: Colors.transparent),
+                                      borderRadius: BorderRadius.circular(30),
+                                    ),
+                                    focusedBorder: OutlineInputBorder(
+                                      borderSide: const BorderSide(color: Colors.transparent),
+                                      borderRadius: BorderRadius.circular(30),
+                                    ),
                                   ),
-                                  focusedBorder: OutlineInputBorder(
-                                    borderSide: const BorderSide(color: Colors.transparent),
-                                    borderRadius: BorderRadius.circular(30),
-                                  ),
-                                ),
-                                value: chooseCategory,
-                                items: _categoriesList.map(
+                                  value: chooseCategory,
+                                  items: _categoriesList.map(
                                         (category) =>
                                         DropdownMenuItem(
                                           value: category,
@@ -146,22 +157,74 @@ class _HomeState extends State<Home>{
                                             ),
                                           ),
                                         ),
-                                ).toList(),
-                                onChanged: (val) {
-                                  setState(() {
-                                    chooseCategory = val as String;
-                                    _selectedCategory = chooseCategory;
-                                  });
-                                },
+                                  ).toList(),
+                                  onChanged: (val) {
+                                    setState(() {
+                                      chooseCategory = val as String;
+                                      _selectedCategory = chooseCategory;
+                                    });
+                                  },
+                                ),
                               ),
-                            ),
+                              PopupMenuButton<String>(
+                                color: Colors.white.withOpacity(0.9),
+                                surfaceTintColor: primaryColor,
+                                shape: const RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(10.0),
+                                  ),
+                                ),
+                                onSelected: (value) {
+                                  if (value == 'Add category') {
+                                    _showAddCategoryDialog(context);
+                                  } else if (value == 'Delete category') {
+
+                                  }
+                                },
+                                itemBuilder: (BuildContext context) {
+                                  return [
+                                    const PopupMenuItem<String>(
+                                        value: 'Add category',
+                                        child: Text(
+                                          'Add category',
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 15
+                                          ),
+                                        )
+                                    ),
+                                    const PopupMenuItem<String>(
+                                        value: 'Delete category',
+                                        child: Text(
+                                          'Delete category',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 15
+                                          ),
+                                        )
+                                    ),
+                                  ];
+                                },
+                                child: Container(
+                                  height: 40,
+                                  width: 15,
+                                  alignment: Alignment.centerRight,
+                                  child: const Icon(
+                                    Icons.more_vert,
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                         ],
                       ),
                     ],
                   ),
                   Expanded(
-                    child: TasksList(category: _selectedCategory),
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 10),
+                      child: TasksList(category: _selectedCategory),
+                    ),
                   ),
                 ],
               ),
